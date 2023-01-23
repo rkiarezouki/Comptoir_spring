d48fd8e2d1d118952f719284155aaf9fd7ace612
rkiarezouki <rkia.rezouki@etud.univ-jfc.fr>
2023-01-23 16:09:05 +0200

    commitx2

--- src/test/java/comptoirs/service/LigneServiceTest.java
+++ src/test/java/comptoirs/service/LigneServiceTest.java
@@ -20,6 +20,7 @@ class LigneServiceTest {
     static final int REFERENCE_PRODUIT_DISPONIBLE_4 = 96;
     static final int REFERENCE_PRODUIT_INDISPONIBLE = 97;
     static final int UNITES_COMMANDEES_AVANT = 0;
+    static final int REFERENCE_PRODUIT_INEXISTANT = 99999;
 
     @Autowired
     LigneService service;
@@ -37,4 +38,29 @@ class LigneServiceTest {
             () -> service.ajouterLigne(NUMERO_COMMANDE_PAS_LIVREE, REFERENCE_PRODUIT_DISPONIBLE_1, 0),
             "La quantite d'une ligne doit être positive");
     }
+
+    @Test
+    void leProduitexiste(){
+        assertThrows(Exception.class, () -> service.ajouterLigne(NUMERO_COMMANDE_PAS_LIVREE, REFERENCE_PRODUIT_INEXISTANT, 1),
+                "Le produit était inexistant");
+    }
+
+    @Test
+    void leProduitEstDisponible(){
+        assertThrows(Exception.class, () ->  service.ajouterLigne(NUMERO_COMMANDE_PAS_LIVREE, REFERENCE_PRODUIT_INDISPONIBLE, 1),
+                "Le produit n'est pas disponible");
+    }
+
+    @Test
+    void laCommandeEstPasLivree(){
+        assertThrows(Exception.class, () -> service.ajouterLigne(NUMERO_COMMANDE_DEJA_LIVREE,REFERENCE_PRODUIT_DISPONIBLE_1, 1),
+                "La commande était déjà livrée");
+    }
+
+    @Test
+    void quantiteSuffisante(){
+        assertThrows(Exception.class, () -> service.ajouterLigne((NUMERO_COMMANDE_PAS_LIVREE),REFERENCE_PRODUIT_DISPONIBLE_1, 10000),
+                "la quantité commandée était supérieure au stock");
+    }
+
 }
